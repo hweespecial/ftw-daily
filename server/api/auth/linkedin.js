2b113eea944e46babf55212fc9072c2a744b373b
Jenni Laakso <jenni@sharetribe.com>
2020-11-25 10:01:11 +0200

    Example: add Linkedin by using passport.js

---
+++ server/api/auth/linkedin.js
@@ -0,0 +1,104 @@
+const passport = require('passport');
+var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+const loginWithIdp = require('./loginWithIdp');
+const { createIdToken } = require('../../api-util/idToken');
+
+const radix = 10;
+const PORT = parseInt(process.env.REACT_APP_DEV_API_SERVER_PORT, radix);
+const rootUrl = process.env.REACT_APP_CANONICAL_ROOT_URL;
+const clientID = process.env.REACT_APP_LINKEDIN_CLIENT_ID;
+const clientSecret = process.env.LINKEDIN_CLIENT_SECRET;
+
+let callbackURL = null;
+
+const useDevApiServer = process.env.NODE_ENV === 'development' && !!PORT;
+
+if (useDevApiServer) {
+  callbackURL = `http://localhost:${PORT}/api/auth/linkedin/callback`;
+} else {
+  callbackURL = `${rootUrl}/api/auth/linkedin/callback`;
+}
+
+const strategyOptions = {
+  clientID,
+  clientSecret,
+  callbackURL,
+  scope: ['r_emailaddress', 'r_liteprofile'],
+  passReqToCallback: true,
+};
+
+const verifyCallback = (req, accessToken, refreshToken, rawReturn, profile, done) => {
+  // We can can use util function to generate id token to match OIDC so that we can use
+  // our custom id provider in Flex
+
+  console.log('Profile', profile);
+
+  const locale = Object.keys(profile._json.firstName.localized)[0];
+
+  const firstName = profile._json.firstName.localized[locale];
+  const lastName = profile._json.lastName.localized[locale];
+  const email = profile.emails[0].value;
+
+  const user = {
+    userId: profile.id,
+    profile: {
+      firstName,
+      lastName,
+      email,
+    },
+  };
+
+  const state = req.query.state;
+  const queryParams = JSON.parse(state);
+
+  const { from, defaultReturn, defaultConfirm } = queryParams;
+
+  const idpToken = createIdToken(user)
+    .then(idpToken => {
+      const userData = {
+        email,
+        firstName,
+        lastName,
+        idpToken,
+        from,
+        defaultReturn,
+        defaultConfirm,
+      };
+
+      console.log('userData:', userData);
+
+      done(null, userData);
+    })
+    .catch(e => console.error(e));
+};
+
+// ClientId is required when adding a new Linkedin strategy to passport
+if (clientID) {
+  passport.use(new LinkedInStrategy(strategyOptions, verifyCallback));
+}
+
+exports.authenticateLinkedin = (req, res, next) => {
+  const from = req.query.from ? req.query.from : null;
+  const defaultReturn = req.query.defaultReturn ? req.query.defaultReturn : null;
+  const defaultConfirm = req.query.defaultConfirm ? req.query.defaultConfirm : null;
+
+  const params = {
+    ...(!!from && { from }),
+    ...(!!defaultReturn && { defaultReturn }),
+    ...(!!defaultConfirm && { defaultConfirm }),
+  };
+
+  const paramsAsString = JSON.stringify(params);
+
+  passport.authenticate('linkedin', {
+    state: paramsAsString,
+  })(req, res, next);
+};
+
+// Use custom callback for calling loginWithIdp enpoint
+// to log in the user to Flex with the data from Linkedin
+exports.authenticateLinkedinCallback = (req, res, next) => {
+  passport.authenticate('linkedin', function(err, user) {
+    loginWithIdp(err, user, req, res, clientID, 'linkedin');
+  })(req, res, next);
+};
